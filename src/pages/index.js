2ad8fbc473928a763c4a3933712f046561991f3a
Jianmin <nicklee19950330@gmail.com>
2020-08-20 23:05:00 -0500

    blog application using gatsby

---
+++ src/pages/index.js
@@ -0,0 +1,60 @@
+import React from "react"
+import { graphql, Link } from "gatsby"
+import styled from 'styled-components'
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const BlogLink = styled(Link)`
+  text-decoration: none;
+`
+
+const BlogTitle = styled.h3`
+  margin-bottom: 20px;
+  color: blue;
+`
+
+export default ({ data }) => {
+  return (
+    <Layout>
+      <SEO title='home' />
+      <div>
+      <h1>Jianmin's Thoughts</h1>
+      <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+      {data.allMarkdownRemark.edges.map(({ node }) =>(
+        <div key={node.id}>
+          <BlogLink to={node.fields.slug}>
+          <BlogTitle>
+            {node.frontmatter.title} - {node.frontmatter.date}
+          </BlogTitle>
+          </BlogLink>
+          <p>{node.except}</p>
+
+        </div>
+      ))}
+      </div>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query MyQuery {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            description
+            title
+            date
+          }
+          fields {
+            slug
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
